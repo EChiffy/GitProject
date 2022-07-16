03430732ed3dfe72fb998f2eb7d6b353defca5cb
Echiffy <ericchiffolo@gmail.com>
2022-07-16 14:05:45 -0400

    added cdk dependencies

--- cdkproject/lib/cdkproject-stack.ts
+++ cdkproject/lib/cdkproject-stack.ts
@@ -1,6 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
 
 export class CdkprojectStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
